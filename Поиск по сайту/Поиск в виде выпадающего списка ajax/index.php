349b36585983e947d8ddd3f085d527aa8a6d7d11
RecentForce99 <76057851+RecentForce99@users.noreply.github.com>
2022-08-16 21:58:28 +0300

    Delete Поиск по сайту directory

--- Поиск по сайту/Поиск в виде выпадающего списка ajax/index.php
+++
@@ -1,6 +0,0 @@
-<form class="header__search-block-inner">
-    <label>
-        <input type="search" class="global-search-ajax">
-    </label>
-    <button class="all-btn"><span>Поиск</span></button>
-</form>
