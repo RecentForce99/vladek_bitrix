51645bce1b94817eafe302f8d3aace7da44c420d
RecentForce99 <76057851+RecentForce99@users.noreply.github.com>
2022-08-31 00:01:14 +0300

    Vladek

---
+++ Битрикс/Заготовки/Подключение include файла.php
@@ -0,0 +1,14 @@
+   <?$APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            Array(
+                                "AREA_FILE_SHOW" => "file",
+                                "AREA_FILE_SUFFIX" => "inc",
+                                "COMPOSITE_FRAME_MODE" => "A",
+                                "COMPOSITE_FRAME_TYPE" => "AUTO",
+                                "EDIT_TEMPLATE" => "",
+                                "PATH" => "/include/main-screen/index.php",
+                                'ID' => 12
+                            )
+                        );
+                        ?>
