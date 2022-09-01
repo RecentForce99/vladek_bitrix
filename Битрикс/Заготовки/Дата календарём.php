0e5040a666aa4a64e45a265357ee1fa04baf87c0
RecentForce99 <76057851+RecentForce99@users.noreply.github.com>
2022-09-01 22:09:58 +0300

    Vladek

--- Битрикс/Заготовки/Дата календарём.php
+++ Битрикс/Заготовки/Дата календарём.php
@@ -1,1 +1,6 @@
-      <input placeholder="01.01.1970" type="text" name="BIRTHDAY" onclick="BX.calendar({node: this, field: this, bTime: false});">
+<?php CJSCore::Init(array("jquery","date"));?>
+<input placeholder="01.01.1970" type="text" name="BIRTHDAY" onclick="BX.calendar({node: this, field: this, bTime: false});">
+
+
+
+Подробнее https://dev.1c-bitrix.ru/api_help/js_lib/data/calendar.php
