401b50674e052491ef6167da712632bc88011c42
RecentForce99 <76057851+RecentForce99@users.noreply.github.com>
2022-10-11 19:18:09 +0300

    Vladek

--- Битрикс/Заготовки/Обрезка строки.php
+++ Битрикс/Заготовки/Обрезка строки.php
@@ -8,4 +8,37 @@
         $tmp = mb_substr($str, 0, $length, $encoding);
         return mb_substr($tmp, 0, mb_strripos($tmp, ' ', 0, $encoding), $encoding) . $postfix;
     };
+
+function cutStringToDotOrComma($str, $length, $encoding='UTF-8') : string
+{
+    $str = strip_tags($str);
+    if (mb_strlen($str, $encoding) <= $length) 
+        return $str;
+
+    $tmp = mb_substr($str, 0, $length, $encoding);
+    $croppedStr = mb_substr($tmp, 0, mb_strripos($tmp, ' ', 0, $encoding), $encoding);
+
+    $strToArrayTransform = explode(' ', $croppedStr);
+    $result = '';
+
+    for( $i = count($strToArrayTransform); $i > 0; $i-- )
+    {
+        if(isset($strToArrayTransform[$i]))
+        {
+            if(mb_substr($strToArrayTransform[$i], -1) == '.' || mb_substr($strToArrayTransform[$i], -1) == ',')
+            {
+                $result = implode(' ', $strToArrayTransform); 
+                break;
+            }
+            else unset($strToArrayTransform[$i]);   
+        }
+    }
+
+    if(!$result) $result = $croppedStr;
+
+    return $result;
+}
+
+
+
 ?> 
