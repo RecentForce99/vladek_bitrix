90682fa191d0f4b37cda350fde7eddc5eeb87d1b
RecentForce99 <76057851+RecentForce99@users.noreply.github.com>
2022-10-12 13:20:05 +0300

    Vladek

--- Битрикс/Заготовки/Обрезка строки.php
+++ Битрикс/Заготовки/Обрезка строки.php
@@ -1,9 +1,8 @@
 <?php
  function mbCutString($str, $length, $postfix='...', $encoding='UTF-8')
     {
-        if (mb_strlen($str, $encoding) <= $length) {
+        if (mb_strlen($str, $encoding) <= $length)
             return $str;
-        }
 
         $tmp = mb_substr($str, 0, $length, $encoding);
         return mb_substr($tmp, 0, mb_strripos($tmp, ' ', 0, $encoding), $encoding) . $postfix;
