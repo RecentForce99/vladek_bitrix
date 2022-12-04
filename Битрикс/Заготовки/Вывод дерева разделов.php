22aa6450fa4f5931b0bd17dc0a188ee34a4ac641
Nikita Potorokin <potorokin.n@aicrobotics.ru>
2022-12-04 18:58:35 +0300

    Vladek

--- Битрикс/Заготовки/Вывод дерева разделов.php
+++ Битрикс/Заготовки/Вывод дерева разделов.php
@@ -1,17 +1,36 @@
 <?php
-    $arFilter = array(
-                'ACTIVE' => 'Y',
-                'IBLOCK_ID' => 42,
-                'GLOBAL_ACTIVE'=>'Y',
-            );
-            $arSelect = array('IBLOCK_ID','ID','NAME','DEPTH_LEVEL','IBLOCK_SECTION_ID', 'UF_LINK');
-            $arOrder = array('DEPTH_LEVEL'=>'ASC','SORT'=>'ASC');
-            $rsSections = CIBlockSection::GetList($arOrder, $arFilter, false, $arSelect);
-            $sectionLinc = array();
-            $arResult['ROOT'] = array();
-            $sectionLinc[0] = &$arResult['ROOT'];
-            while($arSection = $rsSections->GetNext()) {
-                $sectionLinc[intval($arSection['IBLOCK_SECTION_ID'])]['CHILD'][$arSection['ID']] = $arSection;
-                $sectionLinc[$arSection['ID']] = &$sectionLinc[intval($arSection['IBLOCK_SECTION_ID'])]['CHILD'][$arSection['ID']];
-            }
+
+$elements = $this->getElements();
+$sectionsTable = SectionTable::getList(
+    [
+        'select' => [
+            'IBLOCK_ID',
+            'ID',
+            'NAME',
+            'DEPTH_LEVEL',
+            'IBLOCK_SECTION_ID',
+        ],
+        'filter' => [
+            'ACTIVE' => 'Y',
+            'IBLOCK_ID' => $this->iblockId,
+            'GLOBAL_ACTIVE' => 'Y',
+            '>=LEFT_MARGIN' => $this->leftMargin,
+            '<=RIGHT_MARGIN' => $this->rightMargin,
+        ],
+        'order' => [
+            'DEPTH_LEVEL' => 'ASC',
+            'SORT' => 'ASC'
+        ],
+        'cache' => $this->cacheTime,
+    ]
+);
+while ($section = $sectionsTable->fetch()) {
+    $sectionID = intval($section['IBLOCK_SECTION_ID']);
+
+    if ($section['DEPTH_LEVEL'] == 3) {
+        $section['ELEMENTS'] = $elements[$section['ID']];
+    }
+    $sections[$sectionID]['CHILD'][$section['ID']] = $section;
+    $sections[$section['ID']] = &$sections[$sectionID]['CHILD'][$section['ID']];
+}
 ?>
